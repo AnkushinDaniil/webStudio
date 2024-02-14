f67aa3a93faf648002279b9838da37558bf12008
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-02-14 12:15:37 +0300

    authorization added

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -23,5 +23,23 @@ func (h *Handler) signUp(c *gin.Context) {
 	c.JSON(http.StatusOK, map[string]interface{}{"id": id})
 }
 
+type sighInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func (h *Handler) signIn(c *gin.Context) {
+	var input sighInInput
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{"token": token})
 }
--- internal/service/auth.go
+++ internal/service/auth.go
@@ -2,13 +2,24 @@ package service
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/sha3"
 	"main.go/internal/entity"
 	"main.go/internal/repository"
 )
 
-const SALT = "57ct480 (T^&(6 n79TG789)"
+const (
+	salt       = "57ct480 (T^&(6 n79TG789)"
+	sighingKey = "v9R^V(&6&V*r^8^"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.RegisteredClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -23,9 +34,23 @@ func (s *AuthService) CreateUser(user entity.User) (int, error) {
 	return s.repo.CreateUser(user)
 }
 
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{jwt.RegisteredClaims{
+		ExpiresAt: &jwt.NumericDate{time.Now().Add(tokenTTL)},
+		IssuedAt:  &jwt.NumericDate{time.Now()},
+	}, user.Id})
+
+	return token.SignedString([]byte(sighingKey))
+}
+
 func generatePasswordHash(password string) string {
 	hash := sha3.New256()
 	hash.Write(([]byte(password)))
 
-	return fmt.Sprintf("%x", hash.Sum([]byte(SALT)))
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
 }
