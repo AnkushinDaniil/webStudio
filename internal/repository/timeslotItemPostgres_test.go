29268964dfea34e8e4f16182efac6745c441d413
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-02-23 18:02:06 +0300

    TestTimeslotItemPostgres_GetAll was added

--- internal/repository/timeslotItemPostgres_test.go
+++ internal/repository/timeslotItemPostgres_test.go
@@ -124,3 +124,107 @@ func TestTimeslotItemPostgres_Create(t *testing.T) {
 		})
 	}
 }
+
+func TestTimeslotItemPostgres_GetAll(t *testing.T) {
+	dataBase, mock, err := sqlmock.Newx()
+	if err != nil {
+		t.Errorf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer dataBase.Close()
+
+	rep := NewTimeslotItemPostgres(dataBase)
+
+	type input struct {
+		listID int
+		userID int
+	}
+
+	timeNow := time.Now()
+
+	type mockBehavior func()
+
+	testTable := []struct {
+		name         string
+		mockBehavior mockBehavior
+		input        input
+		want         []entity.TimeslotItem
+		wantErr      bool
+	}{
+		{
+			name: "OK",
+			mockBehavior: func() {
+				rows := sqlmock.NewRows([]string{"id", "title", "description", "beginning", "finish", "done"}).
+					AddRow(1, "title1", "description1", timeNow, timeNow, true).
+					AddRow(2, "title2", "description2", timeNow, timeNow, false).
+					AddRow(3, "title3", "description3", timeNow, timeNow, false)
+
+				mock.ExpectQuery(`SELECT (.+) FROM timeslots_items ti INNER JOIN lists_items li on (.+) 
+									INNER JOIN users_lists ul on (.+) WHERE (.+)`).
+					WithArgs(1, 1).
+					WillReturnRows(rows)
+			},
+			input: input{
+				listID: 1, userID: 1,
+			},
+
+			want: []entity.TimeslotItem{
+				{
+					ID:          1,
+					Title:       "title1",
+					Description: "description1",
+					Start:       timeNow,
+					End:         timeNow,
+					Done:        true,
+				},
+				{
+					ID:          2,
+					Title:       "title2",
+					Description: "description2",
+					Start:       timeNow,
+					End:         timeNow,
+					Done:        false,
+				},
+				{
+					ID:          3,
+					Title:       "title3",
+					Description: "description3",
+					Start:       timeNow,
+					End:         timeNow,
+					Done:        false,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "No Records",
+			mockBehavior: func() {
+				rows := sqlmock.NewRows(
+					[]string{"id", "title", "description", "beginning", "finish", "done"},
+				)
+				mock.ExpectQuery(`SELECT (.+) FROM timeslots_items ti INNER JOIN lists_items li on (.+) 
+									INNER JOIN users_lists ul on (.+) WHERE (.+)`).
+					WithArgs(1, 1).
+					WillReturnRows(rows)
+			},
+			input: input{
+				listID: 1,
+				userID: 1,
+			},
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.mockBehavior()
+
+			got, err := rep.GetAll(testCase.input.userID, testCase.input.listID)
+			if testCase.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, testCase.want, got)
+			}
+			assert.NoError(t, mock.ExpectationsWereMet())
+		})
+	}
+}
